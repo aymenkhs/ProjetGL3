8b0ee51dee875030ca12351a8d57275a018a53da
aymen <45071818+aymenkhs@users.noreply.github.com>
2020-09-22 23:05:20 +0100

    Ascenseur working on it

--- src/Ascenseur.java
+++ src/Ascenseur.java
@@ -6,19 +6,52 @@ public class Ascenseur {
     private ArrayList<Porte> list_portes;
 
     private int etageCourrant;
-    private int destination;
     private String direction;
+    private ArrayList<Usager> list_destinations;
     private ArrayList<Usager> list_appels;
 
-    public Ascenseur(int nb_etages) {
+    public Ascenseur(int nb_etages, ArrayList<Porte> list_portes) {
         this.nb_etages = nb_etages;
-        this.list_portes = new ArrayList<Porte>();
-        for(int i=0; i<=nb_etages;i++){
-            this.list_portes.add(new Porte(i));
-        }
+        this.list_portes = list_portes;
 
         this.etageCourrant = 0;
         this.direction = "none";
         this.list_appels = new ArrayList<Usager>();
+        this.list_destinations = new ArrayList<Usager>();
+    }
+
+    public void ajouterAppel(Usager u){
+        list_appels.add(u);
+    }
+
+    public void updateDirection(){
+        /*
+            we update the direction after adding a new call from the user or after arriving on the destination based on the rules of the TP
+        */
+
+        // here maybe we should put the direction to "none" cause there's no direction, or we should remove the direction entirely
+        if (etageCourrant ==  nb_etages){
+            direction = "down";
+        }else if(etageCourrant ==  0){
+            direction = "up";
+        }
+    }
+
+    public void deplacer(){
+        /*
+            we move until we arrive at the destination
+        */
+        if (direction.equals("none")){
+            return;
+        }
+
+        if (direction.equals("up")){
+            etageCourrant ++;
+        } else if (direction.equals("down")){
+            etageCourrant --;
+        }
+        System.out.println("+ Ascenseur arriver à l'etage n°" + etageCourrant);
+        // chezck if the current etage has anyone moving from the elevator in it
+        // chezck if the current etage has anyone entering the elevator in it 
     }
 }
