35dfda89334fc35adec05d0b5f1312aa4773ae43
aymen <45071818+aymenkhs@users.noreply.github.com>
2020-09-22 22:12:39 +0100

    Ascenseur init

--- src/Ascenseur.java
+++ src/Ascenseur.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Ascenseur {
+
+    private int nb_etages;
+    private ArrayList<Porte> list_portes;
+
+    private int etageCourrant;
+    private int destination;
+    private String direction;
+    private ArrayList<Usager> list_appels;
+
+    public Ascenseur(int nb_etages) {
+        this.nb_etages = nb_etages;
+        this.list_portes = new ArrayList<Porte>();
+        for(int i=0; i<=nb_etages;i++){
+            this.list_portes.add(new Porte(i));
+        }
+
+        this.etageCourrant = 0;
+        this.direction = "none";
+        this.list_appels = new ArrayList<Usager>();
+    }
 }
--- src/Porte.java
+++ src/Porte.java
@@ -1,12 +1,26 @@
 public class Porte {
 
     private int etage;
+    private String status; // "closed" or "open"
 
     public Porte(int etage) {
         this.etage = etage;
+        this.status = "closed";
     }
 
     public int getEtage() {
         return etage;
+    }
+
+    public boolean isTheDoorClosed() {
+        return status.equals("closed");
+    }
+
+    public void closeTheDoor() {
+        this.status = "closed";
+    }
+
+    public void openTheDoor() {
+        this.status = "open";
     }
 }
