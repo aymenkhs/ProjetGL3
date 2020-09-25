c8f8f6a524a763e6f373422a7f7ed94a74de003e
youcefAZ <youcefdota@gmail.com>
2020-09-25 14:43:05 +0100

    added tests for usager and porte

--- tests/PorteTest.java
+++ tests/PorteTest.java
@@ -1,22 +1,36 @@
+import enums.Status;
 import org.junit.Test;
 
+import static enums.Status.Closed;
 import static org.junit.Assert.*;
 
 public class PorteTest {
 
     @Test
     public void getEtage() {
+
+        System.out.println("Test : getEtage");
+
+        Porte instance= new Porte(3);
+        int expresult= 3;
+        int result=instance.getEtage();
+        assertEquals(expresult,result);
     }
 
     @Test
     public void isTheDoorClosed() {
-    }
 
-    @Test
-    public void closeTheDoor() {
-    }
+        System.out.println("Test : isTheDoorClosed");
 
-    @Test
-    public void openTheDoor() {
+        Porte instance= new Porte(4);
+        boolean expResult= true;
+        instance.closeTheDoor();
+        boolean res= instance.isTheDoorClosed();
+        assertEquals(expResult,res);
+
+        instance.openTheDoor();
+        expResult=false;
+        res= instance.isTheDoorClosed();
+        assertEquals(expResult,res);
     }
 }
--- tests/UsagerTest.java
+++ tests/UsagerTest.java
@@ -1,3 +1,4 @@
+import enums.Direction;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,29 +7,35 @@ public class UsagerTest {
 
     @Test
     public void getEtageCourrant() {
+
+        System.out.println("Test : getEtageCourrant");
+
+        Usager instance= new Usager(4,Direction.Up,6);
+        int expresult=4;
+        int res= instance.getEtageCourrant();
+        assertEquals(expresult,res);
     }
 
     @Test
     public void getDestination() {
+
+        System.out.println("Test : getDestination");
+
+        Usager instance= new Usager(4,Direction.Up,6);
+        int expresult=6;
+        int res= instance.getDestination();
+        assertEquals(expresult,res);
     }
 
     @Test
     public void getDirection() {
+
+        System.out.println("Test : getDirection");
+
+        Usager instance= new Usager(4,Direction.Up,6);
+        Direction expresult=Direction.Up;
+        Direction res= instance.getDirection();
+        assertEquals(expresult,res);
     }
 
-    @Test
-    public void prcessus_dappel() {
-    }
-
-    @Test
-    public void entrer() {
-    }
-
-    @Test
-    public void signaler_destination() {
-    }
-
-    @Test
-    public void sortir() {
-    }
 }
