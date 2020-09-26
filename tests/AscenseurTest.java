a1c8757754c476d2e72ee0bc73f716bdc01753ae
youcefAZ <youcefdota@gmail.com>
2020-09-26 18:56:59 +0100

    started ascenseur tests

--- tests/AscenseurTest.java
+++ tests/AscenseurTest.java
@@ -1,18 +1,30 @@
+import enums.Direction;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class AscenseurTest {
 
     @Test
-    public void ajouterAppel() {
-    }
+    public void testAscenseur() {
 
-    @Test
-    public void updateDirection() {
-    }
+        ArrayList<Porte> doorList= new ArrayList<>();
+        for(int i=0;i<10;i++){
+            Porte porte= new Porte(i);
+            doorList.add(porte);
+        }
 
-    @Test
-    public void deplacer() {
+        Ascenseur instance= new Ascenseur(10,doorList);
+
+        instance.ajouterAppel(new Usager(2,Direction.Up,5));
+        instance.ajouterAppel(new Usager(1,Direction.Up,3));
+        instance.ajouterAppel(new Usager(6,Direction.Down,4));
+        instance.ajouterAppel(new Usager(4,Direction.Down,1));
+        instance.ajouterAppel(new Usager(2,Direction.Up,9));
+
+        instance.deplacer();
+        instance.deplacer();
     }
 }
